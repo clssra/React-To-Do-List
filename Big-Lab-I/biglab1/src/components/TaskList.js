39cbeae8ca500af97cfa4f29feb75b29d55f2f66
clssrx <saracolosio@icloud.com>
2022-03-29 16:39:19 +0200

    add dynamic tasks

--- Big-Lab-I/biglab1/src/components/TaskList.js
+++ Big-Lab-I/biglab1/src/components/TaskList.js
@@ -4,75 +4,101 @@ import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import { PencilSquare, TrashFill, PersonSquare, PlusCircleFill } from 'react-bootstrap-icons';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import dayjs from 'dayjs';
+import isYesterday from 'dayjs/plugin/isYesterday';
+import isTomorrow from 'dayjs/plugin/isTomorrow';
+import isToday from 'dayjs/plugin/isToday';
 
-function TaskList(){
+dayjs.extend(isYesterday).extend(isToday).extend(isTomorrow);
+
+/* TASKLIST
+
+ { id: 1, description: "Complete BigLab 1C", important: false, private: true, deadline: dayjs("2021-04-26T14:30:00") },
+
+    id - description - important - private - deadline
+*/
+
+const formatDeadline = (d) => {
+    if (!d) return '--o--';
+    else if (d.isToday()) {
+      return d.format('[Today at] HH:mm');
+    } else if (d.isTomorrow()) {
+      return d.format('[Tomorrow at] HH:mm');
+    } else if (d.isYesterday()) {
+      return d.format('[Yesterday at] HH:mm');
+    } else {
+      return d.format('dddd DD MMMM YYYY [at] HH:mm');
+    }
+  }
+
+
+function TaskList(props){
+
+    const [tasks, setTasks] = useState(props.tasks);
+    tasks.forEach(t => console.log(t));
+   
     return(
         <Container fluid>
             <h1>FILTER: filter-all</h1>
-                    <Row className='justify-content-between task'>
-                        <Col>
-                        <Form>
-                            <Form.Check type='checkbox' id='btncheck1' label='Complete Lab 2'>
-                            </Form.Check>
-                        </Form>
-                        </Col>
-                        <Col sm={4}>
-                        <small> Monday 22 March 2021 at 14:30</small>
-                        </Col>
-                        <Col sm={1}>
-                            <PencilSquare size='15' />
-                        </Col>
-                        <Col sm={1}>
-                            <TrashFill size='15' />
-                        </Col>
-               
-                    </Row>
-                    <Row className='justify-content-between task'>
-                        <Col >
-                            <Form>
-                                <Form.Check type='checkbox' id='btncheck2' label='Buy some groceries'>
-                                </Form.Check>
-                            </Form>
-                        </Col>
-                        <Col sm={1}>
-                            <PersonSquare size="15" />
-                        </Col>
-                        <Col sm={4}>
-                            <small> Today at 14:30</small>
-                        </Col>
-                        <Col sm={1}>
-                            <PencilSquare size='15' />
-                        </Col>
-                        <Col sm={1}>
-                            <TrashFill size='15' />
-                        </Col>
-                    </Row>
-
-                    <Row className='justify-content-between task'>
-                        <Col>
-                            <Form>
-                                <Form.Check type='checkbox' id='btncheck3' label='Read a good book!' className='important'>
-                                </Form.Check>
-                            </Form>
-                        </Col>
-                        <Col sm={4} md='center'>
-                            <small>--o--</small>
-                        </Col>
-                        <Col sm={1}>
-                            <PencilSquare size='15' />
-                        </Col>
-                        <Col sm={1}>
-                            <TrashFill size='15' />
-                        </Col>
-                    </Row>
-
+                
+            {tasks.map(task => <TaskRow key={task.id} task={task}/>)}
                    
-                    <PlusCircleFill size={30} className='addButton' />
-                    
-             
+            <PlusCircleFill size={30} className='addButton' />
+                     
         </Container>
     );
+}
+
+function TaskRow(props){
+
+    return(
+        <Row className='justify-content-between task'>
+            <TaskInfo {...props}/>
+            <TaskControl/>
+        </Row>
+    );
+
+}
+
+function TaskInfo(props){
+
+    const { task } = props;
+
+    return(
+        <>
+            <Col>
+                <Form>
+                    <Form.Check type='checkbox' id={'btncheck'+ task.id} label={task.description}>
+                    </Form.Check>
+                </Form>
+            </Col>
+
+            <Col sm={1}>
+                <PersonSquare size="15" className={task.private ? 'invisible' : ''}/>
+            </Col>
+
+            <Col sm={4}>
+                <small> {formatDeadline(task.deadline)} </small>
+            </Col>
+        </>
+    );
+}
+
+function TaskControl(props){
+
+    return(
+        <>
+            <Col sm={1}>
+                <PencilSquare size='15' />
+            </Col>
+            <Col sm={1}>
+                <TrashFill size='15' />
+            </Col>
+        </>
+    );
+
 }
 
 export default TaskList;
