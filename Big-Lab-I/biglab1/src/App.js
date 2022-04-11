7d87a85675aa73eb4c9022025cc20526b33af78b
clssrx <saracolosio@icloud.com>
2022-04-12 00:07:42 +0200

    add edit and delete

--- Big-Lab-I/biglab1/src/App.js
+++ Big-Lab-I/biglab1/src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Container from 'react-bootstrap/Container';
+import {Container, Button} from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MyNavbar from './components/MyNavbar';
@@ -8,27 +8,138 @@ import FiltersSidebar from './components/FiltersSidebar';
 import TaskList from './components/TaskList';
 import TASKS from './tasks';
 import { useState } from 'react';
+import dayjs from 'dayjs';
+import isYesterday from 'dayjs/plugin/isYesterday';
+import isTomorrow from 'dayjs/plugin/isTomorrow';
+import isToday from 'dayjs/plugin/isToday';
+import isBetween from 'dayjs/plugin/isBetween';
+import { ModalForm } from './components/ModalForm';
+import { BrowserRouter as Router, Routes} from 'react-router-dom';
+
+dayjs.extend(isYesterday).extend(isToday).extend(isTomorrow).extend(isBetween);
 
 
 function App() {
 
-  // const [taskList, setTaskList] = useState(TASKS);
+  const [selectedKey, setSelectedKey] = useState('all');
+  const [tasks, setTasks] = useState(TASKS);
 
+  const MODAL = {CLOSED : -2, ADD : -1};
+
+  const [selectedTask, setSelectedTask] = useState(MODAL.CLOSED);
+
+  function findTask(id){
+    return tasks.find(t => t.id === id);
+  }
+
+  // const updateTask = (task) => {
+  //   setTasks((oldTasks) => oldTasks.map(t => t.id === task.id ? {...task} : t))
+  // }
+
+  const updateTask = (task) => {
+    setTasks( oldTasks => oldTasks.map( t => t.id === task.id ? {...task} : t) );
+  }
+
+  const addTask = (task) => {
+    const id = Math.max(...tasks.map( t => t.id )) + 1;
+    setTasks((oldTasks) => [...oldTasks, { ...task, id: id }] );
+  }
+
+  const deleteTask = (task) => {
+    setTasks(oldTasks =>  oldTasks.filter(t => t.id != task.id));
+  }
+
+  const handleSaveOrUpdate = (task) => {
+    // if the task has an id it is an update
+    if(task.id) 
+      updateTask(task); 
+    // otherwise it is a new task to add
+    else 
+      addTask(task);
+
+    setSelectedTask(MODAL.CLOSED); 
+  }
+
+  function handleClose(){
+    setSelectedTask(MODAL.CLOSED);
+  }
+
+  function onEdit(task){
+    setSelectedTask(task.id);
+  }
 
   return (
-     <Container fluid>
-      <MyNavbar />
-        <Row className='vheight-100 '>
-          <Col sm={4} className='filters-sidebar below-nav bg-light'>
-            <FiltersSidebar className='below-nav'/>
-          </Col>
-          <Col className='below-nav'>
-            <TaskList tasks={TASKS}/>
-          </Col>
-        </Row>
-
-     </Container>
+    <Router>
+      <Container fluid>
+        <MyNavbar />
+          <Row className='vheight-100 '>
+                <TaskManager 
+                  tasks={tasks} 
+                  setTasks={setTasks} 
+                  selectedKey={selectedKey} 
+                  setSelectedKey={setSelectedKey}
+                  deleteTask={deleteTask}
+                  updateTask={handleSaveOrUpdate}
+                  onEdit = {onEdit}
+                  />
+                <Row>
+                  <Col>
+                <Button size="lg" className="fixed-right-bottom" onClick={() => setSelectedTask(MODAL.ADD)}>+</Button>
+                {/* <ModalForm onClose={handleClose}/> */}
+                </Col>
+                </Row>
+                {(selectedTask !== MODAL.CLOSED) && <ModalForm task={findTask(selectedTask)} onSave={handleSaveOrUpdate} onClose={handleClose}></ModalForm>}
+                
+          </Row>
+      </Container>
+    </Router>
   );
+}
+
+function TaskManager(props){
+
+
+  const {tasks, selectedKey, setSelectedKey, updateTask, selectedTask, setSelectedTask, onEdit} = props;
+
+  const isNextWeek = (t) => {
+    const tomorrow = dayjs().add(1, 'day');
+    const nextWeek = dayjs().add(7, 'day');
+    
+    return t.deadline && t.deadline.isBetween(tomorrow, nextWeek, 'day', '[]');
+  }
+
+  const filtersList = {
+    'all' : {description: 'ALL', id : 'all', filterFn : () => true},
+    'important' : {description: 'IMPORTANT', id : 'important', filterFn : t => t.important},
+    'today' : {description: 'TODAY', id : 'today',  filterFn : t => t.deadline && t.deadline.isToday()},
+    'next7' : { description: 'NEXT WEEK', id : 'next7',  filterFn : t => isNextWeek(t)},
+    'private' : {description: 'PRIVATE', id : 'private',  filterFn : t => t.private}
+  }
+
+    return (
+      <>
+        <Col sm={4} className='filters-sidebar below-nav bg-light'>
+          <FiltersSidebar className='below-nav' filters={filtersList} selectedKey={selectedKey} setSelectedKey={setSelectedKey}/>
+        </Col>
+        <Col className='below-nav'>
+          <Title filterName={filtersList[selectedKey].description}/>
+          <TaskList 
+            tasks={tasks.filter(filtersList[selectedKey].filterFn)}
+            deleteTask={props.deleteTask}
+            updateTask={updateTask}
+            onEdit = {onEdit}
+            />
+        </Col>
+      </>
+    )
+}
+
+function Title(props){
+  const filterName = props.filterName;
+
+
+
+  return <h1>Filter: {filterName}</h1>;
 }
 
 export default App;
--- Big-Lab-I/biglab1/src/components/TaskList.js
+++ Big-Lab-I/biglab1/src/components/TaskList.js
@@ -1,26 +1,19 @@
-import Container from 'react-bootstrap/esm/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
+import { Button, Col, Form, Row, Container} from 'react-bootstrap';
 import { PencilSquare, TrashFill, PersonSquare, PlusCircleFill } from 'react-bootstrap-icons';
-import Button from 'react-bootstrap/Button';
-import { useState } from 'react';
 import dayjs from 'dayjs';
 import isYesterday from 'dayjs/plugin/isYesterday';
 import isTomorrow from 'dayjs/plugin/isTomorrow';
 import isToday from 'dayjs/plugin/isToday';
+import isBetween from 'dayjs/plugin/isBetween';
 
-dayjs.extend(isYesterday).extend(isToday).extend(isTomorrow);
+dayjs.extend(isYesterday).extend(isToday).extend(isTomorrow).extend(isBetween);
 
 /* TASKLIST
-
- { id: 1, description: "Complete BigLab 1C", important: false, private: true, deadline: dayjs("2021-04-26T14:30:00") },
-
-    id - description - important - private - deadline
+ { id - description - important - private - deadline }
 */
 
 const formatDeadline = (d) => {
+
     if (!d) return '--o--';
     else if (d.isToday()) {
       return d.format('[Today at] HH:mm');
@@ -31,21 +24,29 @@ const formatDeadline = (d) => {
     } else {
       return d.format('dddd DD MMMM YYYY [at] HH:mm');
     }
-  }
-
+}
 
 function TaskList(props){
 
-    const [tasks, setTasks] = useState(props.tasks);
-    tasks.forEach(t => console.log(t));
-   
+    // const tasks = props.tasks;
+    // const deleteTask = props.deleteTask;
+
+    const {tasks, deleteTask, updateTask, onEdit} = props;
+
+
     return(
         <Container fluid>
-            <h1>FILTER: filter-all</h1>
-                
-            {tasks.map(task => <TaskRow key={task.id} task={task}/>)}
+
+            {tasks.map(task => 
+                <TaskRow 
+                    key={task.id} 
+                    task={task} 
+                    deleteTask={deleteTask} 
+                    updateTask={updateTask}
+                    onEdit = {onEdit}
+                    />)}
                    
-            <PlusCircleFill size={30} className='addButton' />
+            {/* <PlusCircleFill size={30} className='addButton'/> */}
                      
         </Container>
     );
@@ -53,24 +54,32 @@ function TaskList(props){
 
 function TaskRow(props){
 
+
     return(
         <Row className='justify-content-between task'>
-            <TaskInfo {...props}/>
-            <TaskControl/>
+            <TaskInfo 
+                task={props.task}/>
+            <TaskControl 
+                task={props.task} 
+                deleteTask={props.deleteTask} 
+                updateTask={props.updateTask}
+                onEdit = {props.onEdit}
+                />
         </Row>
     );
 
 }
 
 function TaskInfo(props){
-
-    const { task } = props;
+    
+    const task = props.task;
 
     return(
         <>
             <Col>
                 <Form>
-                    <Form.Check type='checkbox' id={'btncheck'+ task.id} label={task.description}>
+                    <Form.Check type='checkbox' id={'btncheck'+ task.id} label={task.description}
+                        className={task.important ? 'important' : ''}>
                     </Form.Check>
                 </Form>
             </Col>
@@ -91,10 +100,12 @@ function TaskControl(props){
     return(
         <>
             <Col sm={1}>
-                <PencilSquare size='15' />
+                <Button onClick={() => {
+                    props.onEdit(props.task);
+                    }}><PencilSquare size='15' /></Button>
             </Col>
             <Col sm={1}>
-                <TrashFill size='15' />
+                <Button onClick={() => props.deleteTask(props.task)}><TrashFill size='15' /></Button>
             </Col>
         </>
     );
